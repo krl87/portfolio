0699901e462893aebe0f960eb206e7a0b36787b8
krl87 <kayley.luftig@gmail.com>
2016-02-20 21:39:42 -0500

    routing pages

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Home' });
+});
+
+//get about page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About Me' });
+});
+
+//get projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+//get services page
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+//get contacts page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
 });
 
 module.exports = router;
